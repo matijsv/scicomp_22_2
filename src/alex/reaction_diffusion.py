5547f4120c977ab7bfbf0e1f115b7d09863cca7b
HarvestStars <574817827@qq.com>
2025-03-08 01:40:01 +0100

    add gray-scott animations

--- src/alex/reaction_diffusion.py
+++ src/alex/reaction_diffusion.py
@@ -1,6 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.animation import FFMpegWriter
+from IPython.display import HTML
+import matplotlib as mpl
+import imageio_ffmpeg
+import os
+
+
+print(imageio_ffmpeg.get_ffmpeg_exe())
 
 # parameters
 size = 100  # spatial size
@@ -20,6 +28,17 @@ V[center-r:center+r, center-r:center+r] = 0.25
 # noise
 U += np.random.uniform(-0.01, 0.01, (size, size))
 V += np.random.uniform(-0.01, 0.01, (size, size))
+
+def reset_global_UV():
+    global U, V
+    U = np.ones((size, size)) * 0.5
+    V = np.zeros((size, size))
+
+    V[center-r:center+r, center-r:center+r] = 0.25
+
+    # noise
+    U += np.random.uniform(-0.01, 0.01, (size, size))
+    V += np.random.uniform(-0.01, 0.01, (size, size))
 
 def laplacian_dirichlet_strong(Z, U_BC):
     """Compute the Laplacian of the array Z, assuming Dirichlet boundary conditions (Strong Imposition)."""
@@ -86,7 +105,7 @@ def laplacian_PBC(Z, delta=dx):
         np.roll(Z, 1, axis=1) + np.roll(Z, -1, axis=1) - 4 * Z
     ) / (delta ** 2)
 
-def update(boundary_condition='PBC', U_BC_update=0.5, alpha_update=D_v, h_update=dx, dt_update=dt):
+def update(boundary_condition='PBC', U_BC_update=0.5, alpha_update=D_v, h_update=dx, dt_update=dt, f_value = f, k_value = k):
     '''Update the state of the system.'''
     global U, V
     
@@ -106,8 +125,8 @@ def update(boundary_condition='PBC', U_BC_update=0.5, alpha_update=D_v, h_update
     Lv = laplacian(V)
     
     # discretized PDE
-    dUdt = D_u * Lu - U * V**2 + f * (1 - U)
-    dVdt = D_v * Lv + U * V**2 - (f + k) * V
+    dUdt = D_u * Lu - U * V**2 + f_value * (1 - U)
+    dVdt = D_v * Lv + U * V**2 - (f_value + k_value) * V
     
     U += dUdt * dt
     V += dVdt * dt
@@ -139,6 +158,76 @@ def plot_system(boundary_condition="PBC"):
     
     plt.colorbar(im)
     plt.show()
+
+# plot the animation of Gray-Scott model simulation
+def plot_animation(boundary_condition="PBC", observed_variable = "U", f_value = f, k_value = k):
+    reset_global_UV()
+    
+    # set the path to ffmpeg.exe
+    ffmpeg_path = imageio_ffmpeg.get_ffmpeg_exe()
+    if not ffmpeg_path:
+        ffmpeg_path = "C:/ffmpeg/bin/ffmpeg.exe"  # manually set the path to ffmpeg.exe, please check the path on your computer!!!
+    mpl.rcParams['animation.ffmpeg_path'] = ffmpeg_path
+    # mpl.rcParams['animation.embed_limit'] = 200
+    print(f"Using FFmpeg at: {ffmpeg_path}")
+
+    # pick up the observed variable
+    global U, V
+    observed_range = {"U": U, "V": V}
+    if observed_variable not in observed_range:
+        raise ValueError("Invalid observed variable")
+    observed_value = observed_range[observed_variable]
+
+    ###### MP4 version animation ######
+    fig_mp4, ax_mp4 = plt.subplots()
+    im_mp4 = ax_mp4.imshow(V, cmap='inferno', interpolation='bilinear')
+    if boundary_condition == "PBC":
+        ax_mp4.set_title(f"Gray-Scott Model Simulation\nBoundary: Periodic Boundary Condition")
+    else :
+        ax_mp4.set_title(f"Gray-Scott Model Simulation\nBoundary: {boundary_condition}")
+
+    def animate_mp4(i):
+        for _ in range(10):
+            # core update function
+            # remember to set the boundary condition, f, and k values
+            update(boundary_condition=boundary_condition, f_value=f_value, k_value=k_value)
+        im_mp4.set_array(observed_value)
+        return [im_mp4]
+
+    ani_mp4 = animation.FuncAnimation(fig_mp4, animate_mp4, frames=500, interval=50)
+
+    # save the animation as an MP4 file
+    writer = FFMpegWriter(fps=30, metadata=dict(artist='Your Name'), bitrate=1800)
+    os.makedirs("./fig", exist_ok=True)
+    ani_mp4.save(f"./fig/Gray-Scott-{boundary_condition}-observed_{observed_variable}-f_{f_value}-k_{k_value}.mp4", writer=writer)
+    print("MP4 saved, Gray-Scott-model.mp4")
+
+    plt.close(fig_mp4)  # close the figure to avoid showing it in the notebook
+
+    ###### HTML version animation ######
+    reset_global_UV()
+    fig_html, ax_html = plt.subplots()
+    im_html = ax_html.imshow(V, cmap='inferno', interpolation='bilinear')
+    if boundary_condition == "PBC":
+        ax_html.set_title(f"Gray-Scott Model Simulation\nBoundary: Periodic Boundary Condition")
+    else :
+        ax_html.set_title(f"Gray-Scott Model Simulation\nBoundary: {boundary_condition}")
+
+    def animate_html(i):
+        for _ in range(50):
+            # core update function
+            # remember to set the boundary condition, f, and k values
+            update(boundary_condition=boundary_condition, f_value = f, k_value = k)
+        im_html.set_array(observed_value)
+        return [im_html]
+
+    ani_html = animation.FuncAnimation(fig_html, animate_html, frames=500, interval=50)
+
+    plt.colorbar(im_html)
+    
+    return HTML(ani_html.to_jshtml())  # display the animation in the notebook
+
+
 
 if __name__ == '__main__':
     plot_system(boundary_condition="PBC")
